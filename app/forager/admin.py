12d658f4ddd6a9996e13dbf2e65b67527d6ed8f0
Joe Bullard <joekbullard@gmail.com>
2022-12-29 14:59:58 +0000

    register models on admin

--- app/forager/admin.py
+++ app/forager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Record)
+admin.site.register(Image)
+admin.site.register(ImageAlbum)
+admin.site.register(Species)
 
 # Register your models here.
