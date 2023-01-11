95645da347bffefdef929f0947ad92f67488dc00
Joe Bullard <joekbullard@gmail.com>
2023-01-11 23:17:42 +0000

    logout and login after registration

--- app/users/urls.py
+++ app/users/urls.py
@@ -2,12 +2,14 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import SignUpView, CustomUserDetailAPI, CustomUserListAPI
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path('api/users/', CustomUserListAPI.as_view()), # new
     path('api/users/<int:pk>/', CustomUserDetailAPI.as_view()), # new
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- app/users/views.py
+++ app/users/views.py
@@ -1,15 +1,27 @@
 # accounts/views.py
 from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views.generic import CreateView
 from rest_framework import generics
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
 
-class SignUpView(generic.CreateView):
+
+
+class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("home")
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        valid = super(SignUpView, self).form_valid(form)
+        email, password = form.cleaned_data.get('email'), form.cleaned_data.get('password1')
+        new_user = authenticate(email=email, password=password)
+        login(self.request, new_user)
+        return valid
+
 
 class CustomUserListAPI(generics.ListAPIView): # new
     queryset = CustomUser.objects.all()
